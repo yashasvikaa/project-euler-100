de4b01343ece66c39500fc1f84f819f488f3407b
Yashasvika Ahuja <yashasvikaahuja@gmail.com>
2025-06-17 08:35:28 +0530

    Add files via upload

---
+++ problem_2.py
@@ -0,0 +1,12 @@
+def main():
+    sum = 0
+    a, b = 1, 2
+    while b < 4_000_000:
+        if b%2 == 0:
+            sum += b
+        a, b = b, a + b
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
